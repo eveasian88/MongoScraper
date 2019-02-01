8ba6f4f39792a2e7f8d2a72b33dc1d8585047341
Susye Weng-Reeder <weng-reeder@hotmail.com>
2019-01-31 16:03:43 -0800

    server.js and set up

---
+++ models/article.js
@@ -0,0 +1,26 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var ArticleSchema = new Schema({
+    title: {
+        type: String,
+        require: true
+    },
+    link: {
+        type: String,
+        require: true
+    },
+    image: {
+        type: String,
+        require: true
+    },
+    saved: {
+        type: Boolean,
+        default: false
+    }
+});
+
+var Article = mongoose.model("Article", ArticleSchema);
+
+module.exports = Articles;
---
+++ server.js
@@ -0,0 +1,41 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+
+var PORT = process.env.PORT || 3000;
+
+// require models
+var db = require("./models");
+
+// initialize express
+var app = express();
+
+// configure middleware
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+// connect to mongoDB
+mongoose.connect("mongodb://localhost/populate", {
+    useNewUrlParser: true });
+
+var conn = mongoose.connection;
+
+conn.on('error', function(err) {
+    console.log("Moongoose Error");
+})
+conn.once('open', function(err) {
+    console.log("Mongoose Connection Successful");
+})
+
+// routes
+require("./routes/apiRoutes") (app);
+
+// start the server
+app.listen(PORT, function() {
+    console.log("App is listening on port " + PORT + "!");
+});
+
+module.exports = app;
