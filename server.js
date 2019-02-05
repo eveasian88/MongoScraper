b9fb57a96f73a958ed5b527c45d5aef81c86581c
Susye Weng-Reeder <weng-reeder@hotmail.com>
2019-02-04 19:22:19 -0800

    edited server

--- server.js
+++ server.js
@@ -12,13 +12,15 @@ const apiRoutes = require("./routes/apiRoutes");
 
 // DATABASE TOOLS
 const mongoose = require("mongoose");
-const DATABASE_URL = process.env.MONGODB_URI || "mongodb://localhost/populate";
+// const DATABASE_URL = process.env.MONGODB_URI || "mongodb://localhost/populate";
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-// var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 // Configure mongoose with MongoDB
-mongoose.connect( DATABASE_URL, { useNewUrlParser: true, useCreateIndex: true } );
+// mongoose.connect( DATABASE_URL, { useNewUrlParser: true, useCreateIndex: true } );
+
+mongoose.connect(MONGODB_URI)
 const conn = mongoose.connection
 
 conn.on("error", error => console.log( "Mongoose Error" ))
